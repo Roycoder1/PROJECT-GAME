a345bc71e9fef98aee20506e67a3d49fedf1d1cc
BENISTI <benisti@MacBook-Air-de-BENISTI.local>
2022-07-07 12:14:02 +0300

    firststep

--- map.js
+++ map.js
@@ -1,28 +1,31 @@
-function move(){
+let inter;
+let pos = 0;
+let character = document.querySelector('div')
+console.log(character)
+document.addEventListener('keydown', function(event){
 
-	let character = document.querySelector('div')
-	console.log(character)
+	console.log(event.key,event.keyCode)
 
-	let pos = 0;
-	let id = character.addEventListener('keypress' ,s)
+	
 
-}
-function s (){
+	// inter = setInterval(()=>{
+		character.style.top = pos + 'px';
+		pos= pos+10;
+	// },5)
+})
+
+
+document.addEventListener('keyup', function(event){
+
+	console.log(event.key,event.keyCode)
+	clearInterval(inter)
+
+
+})
 
 
 
-	{
-		if (pos ==1440){
-			clearInterval(id)
-		}
-		else {
-			pos++
-			box.style.right = pos+'px';
 
-		}
-	}
-
-}
 
 
 
