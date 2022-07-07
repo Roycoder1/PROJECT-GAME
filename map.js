dbf0f2aeaf98cde2429b481990eac19105ec6e0c
BENISTI <benisti@MacBook-Air-de-BENISTI.local>
2022-07-07 15:55:39 +0300

    add

--- map.js
+++ map.js
@@ -1,18 +1,62 @@
 let inter;
-let pos = 0;
-let character = document.querySelector('div')
-console.log(character)
+// let posBottom = 1;
+let character = document.querySelectorAll('div')[2]
+let posLeftRight = 0;
+// let posLeft = 1;
+let posTopBotom = 0;
+let pos;
+
+
+
+
+
+
+
+
+
 document.addEventListener('keydown', function(event){
 
-	console.log(event.key,event.keyCode)
+	// console.log(event.key,event.keyCode)
 
-	
+	if (event.keyCode == 83) {
+		let pos = 1;
+		character.style.top = posTopBotom  +'px';
+		posTopBotom=posTopBotom+(7*pos);
+	}
+	if(event.keyCode == 87){
+		let pos = -1;
+		character.style.top = posTopBotom  +'px';
+		posTopBotom=posTopBotom+(7*pos);
+	}
+	if (event.keyCode==68) {
 
-	// inter = setInterval(()=>{
-		character.style.top = pos + 'px';
-		pos= pos+10;
-	// },5)
-})
+		let pos = 1;
+		character.style.left = posLeftRight + 'px';
+		posLeftRight= posLeftRight+7;
+	}
+	if(event.keyCode==65){
+		let pos = -1;
+		character.style.left = posLeftRight + 'px';
+		posLeftRight= posLeftRight+ (7*pos);
+
+
+	}
+
+	if (posLeftRight>= 398 && posLeftRight<=399  && posTopBotom >=0  && posTopBotom<=98) {
+		
+
+		console.log(posTopBotom, posLeftRight)
+
+		alert('Whats happening?')
+		return;
+	}
+
+
+
+
+}
+)
+
 
 
 document.addEventListener('keyup', function(event){
@@ -23,6 +67,19 @@ document.addEventListener('keyup', function(event){
 
 })
 
+
+
+
+if (posLeftRight>= 398 && posLeftRight<=498  && posTopBotom >=0  && posTopBotom<=98) {
+
+
+	console.log(posTopBotom, posLeftRight)
+
+	alert('Whats happening?')
+
+
+
+}
 
 
 
